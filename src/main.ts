036441796490d888e69b8b08f4d889baa2eeb095
rahbu <rahmedbuehler@googlemail.com>
2024-03-13 08:51:14 -0500

    Add line breaks

--- src/main.ts
+++ src/main.ts
@@ -19,5 +19,10 @@ export class App {
 bootstrapApplication(App);
 
 /*
-Our project managers have asked us for a simple utility to make their life easier. They want a form where they can enter the number of available developer days in the sprint and then information about multiple possible features. For each feature, they’re willing to provide the basic information about the feature (name, id, description), the number of people who'll be happy with the release of the feature, and the estimated number of developer days it will take to complete the feature. Given all of this, they’d like us to display the selection of features that should be taken into the sprint so that we maximize the number of happy people in the long run.
+Our project managers have asked us for a simple utility to make their life easier. They want a form where they can
+enter the number of available developer days in the sprint and then information about multiple possible features. For
+each feature, they’re willing to provide the basic information about the feature (name, id, description), the number
+of people who'll be happy with the release of the feature, and the estimated number of developer days it will take to
+complete the feature. Given all of this, they’d like us to display the selection of features that should be taken into
+the sprint so that we maximize the number of happy people in the long run.
 */
